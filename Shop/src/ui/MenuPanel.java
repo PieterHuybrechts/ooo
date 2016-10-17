111e009e7d0502d85470849bc81a7da1abcc09f0
Pieter Huybrechts <huybrechtspieter@gmail.com>
2016-10-17 14:54:47 +0200

    MenuPanel done

--- Shop/src/ui/MenuPanel.java
+++ Shop/src/ui/MenuPanel.java
@@ -8,6 +8,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import db.DbException;
+import domain.Customer;
+import domain.DomainException;
 import domain.Game;
 import domain.Movie;
 import domain.Product;
@@ -37,33 +39,45 @@ public class MenuPanel extends JPanel{
 		this.shop=shop;
 		
 		this.addProductButton = new JButton("Add product");
-		this.showProductButton = new JButton("Show Product");
-		this.showRentalPriceButton = new JButton("Show rental Price");
-		this.rentProductButton = new JButton("Rent product");
-		this.returnProductButton = new JButton("Return product");
-		this.repairProductButton = new JButton("Repair product");
-		this.deleteProductButton = new JButton("Delete product");
-		this.addCustomerButton = new JButton("Add customer");
-		this.quitButton = new JButton("quit");
-		
-		//addProductButton.addActionListener(l);
-		
+		addProductButton.addActionListener(new AddProductListener());
 		this.add(addProductButton);
+		
+		this.showProductButton = new JButton("Show Product");
+		showProductButton.addActionListener(new ShowProductListener());
 		this.add(showProductButton);
+		
+		this.showRentalPriceButton = new JButton("Show rental Price");
+		showRentalPriceButton.addActionListener(new ShowRentalPriceListener());
 		this.add(showRentalPriceButton);
+		
+		this.rentProductButton = new JButton("Rent product");
+		rentProductButton.addActionListener(new RentProductButtonListener());
 		this.add(rentProductButton);
+		
+		this.returnProductButton = new JButton("Return product");
+		returnProductButton.addActionListener(new ReturnProductButtonListener());
 		this.add(returnProductButton);
+		
+		this.repairProductButton = new JButton("Repair product");
+		repairProductButton.addActionListener(new RepairProductButtonListener());
 		this.add(repairProductButton);
+		
+		this.deleteProductButton = new JButton("Delete product");
+		deleteProductButton.addActionListener(new DeleteProductButtonListener());
 		this.add(deleteProductButton);
+		
+		this.addCustomerButton = new JButton("Add customer");
+		addCustomerButton.addActionListener(new AddCustomerButtonListener());
 		this.add(addCustomerButton);
+		
+		this.quitButton = new JButton("quit");
+		quitButton.addActionListener(new QuitButtonListener());
 		this.add(quitButton);
 	}
-	
-	private class AddProductListener implements ActionListener{
 
-		
+	private class AddProductListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			/*String title = JOptionPane.showInputDialog("Enter the title:");
+			String title = JOptionPane.showInputDialog("Enter the title:");
 			int id;
 			try{
 				id = Integer.parseInt(JOptionPane.showInputDialog("Enter the id:"));
@@ -76,22 +90,34 @@ public class MenuPanel extends JPanel{
 					p= new Game(id,title,ProductStateEnum.RENTABLE);
 				}
 				
-				this.shop.addProduct(p);
-			}catch(NumberFormatException e){
+				shop.addProduct(p);
+			}catch(NumberFormatException ex){
 				JOptionPane.showMessageDialog(null, "ID input is van foute waarde.", "",JOptionPane.WARNING_MESSAGE);
-			} catch (DbException e) {
-				JOptionPane.showMessageDialog(null, e.getMessage(),"",JOptionPane.WARNING_MESSAGE);
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(),"",JOptionPane.WARNING_MESSAGE);
 			}
-			
-		}*/
+		}
 		
 	}
 	
 	private class ShowProductListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
+			String id = JOptionPane.showInputDialog("Enter the id:");
 			
+			Product p = null;
+			try {
+				p=shop.getProduct(Integer.parseInt(id));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "ID input is van foute waarde.", "",JOptionPane.WARNING_MESSAGE);
+			} catch (DbException ex) {
+				JOptionPane.showMessageDialog(null,ex.getMessage(), "",JOptionPane.WARNING_MESSAGE);
+			}
+			
+			if(p!=null)
+			{
+				JOptionPane.showMessageDialog(null, p.getTitle());
+			}
 		}
 		
 	}
@@ -99,60 +125,130 @@ public class MenuPanel extends JPanel{
 	private class ShowRentalPriceListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
+			String id = JOptionPane.showInputDialog("Enter the id:");
 			
+			Product p = null;
+			try {
+				p=shop.getProduct(Integer.parseInt(id));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "ID input is van foute waarde.", "",JOptionPane.WARNING_MESSAGE);
+			} catch (DbException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage() , "",JOptionPane.WARNING_MESSAGE);
+			}
+			
+			
+			if(p!=null){
+				String daysString = JOptionPane.showInputDialog("Enter the number of days:");
+				int days = Integer.parseInt(daysString);
+				JOptionPane.showMessageDialog(null, p.getPrice(days));
+			}
 		}
 		
 	}
 	private class RentProductButtonListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
+			String id = JOptionPane.showInputDialog("Enter the id:");
 			
+			try {
+				shop.rentProduct(Integer.parseInt(id));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "ID input is van foute waarde.", "",JOptionPane.WARNING_MESSAGE);
+			} catch (DomainException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(), "",JOptionPane.WARNING_MESSAGE);
+			} catch (DbException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage() , "",JOptionPane.WARNING_MESSAGE);
+			}
 		}
 		
 	}
-	private class addProductListener implements ActionListener{
+	private class ReturnProductButtonListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
+			String id = JOptionPane.showInputDialog("Enter the id:");
+			boolean damaged=JOptionPane.showConfirmDialog(null, "Is the item damaged?", "", JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION;
 			
+			try {
+				shop.returnProduct(Integer.parseInt(id),damaged);
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "ID input is van foute waarde.", "",JOptionPane.WARNING_MESSAGE);
+			} catch (DomainException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(), "",JOptionPane.WARNING_MESSAGE);
+			} catch (DbException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage() , "",JOptionPane.WARNING_MESSAGE);
+			}
 		}
 		
 	}
-	private class addProductListener implements ActionListener{
+	private class RepairProductButtonListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
+			String id = JOptionPane.showInputDialog("Enter the id:");
+			
+			try {
+				shop.repairProduct(Integer.parseInt(id));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "ID input is van foute waarde.", "",JOptionPane.WARNING_MESSAGE);
+			} catch (DomainException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(), "",JOptionPane.WARNING_MESSAGE);
+			} catch (DbException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage() , "",JOptionPane.WARNING_MESSAGE);
+			}
 			
 		}
 		
 	}
-	private class addProductListener implements ActionListener{
+	private class DeleteProductButtonListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
+			String id = JOptionPane.showInputDialog("Enter the id:");
 			
+			try {
+				shop.deleteProduct(Integer.parseInt(id));
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(null, "ID input is van foute waarde.", "",JOptionPane.WARNING_MESSAGE);
+			} catch (DomainException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(), "",JOptionPane.WARNING_MESSAGE);
+			} catch (DbException ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage() , "",JOptionPane.WARNING_MESSAGE);
+			}
 		}
 		
 	}
-	private class addProductListener implements ActionListener{
+	private class AddCustomerButtonListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
 			
+			
+			try {
+				int id = Integer.parseInt(JOptionPane.showInputDialog("Id:"));
+				String firstName = JOptionPane.showInputDialog("First name:");
+				String lastName = JOptionPane.showInputDialog("Last name:");
+				String address = JOptionPane.showInputDialog("Address:");
+				String zipCode = JOptionPane.showInputDialog("ZIP code:");
+				String city = JOptionPane.showInputDialog("City:");
+				String eMailAddress = JOptionPane.showInputDialog("EMail address:");
+				boolean subscribed = JOptionPane.showConfirmDialog(null, "Subscribe to newsletter?", "", JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION;
+				Customer c = new Customer(id, firstName, lastName, address, zipCode, city, eMailAddress, subscribed);
+				shop.addCustomer(c);
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(),"",JOptionPane.WARNING_MESSAGE);
+			}
 		}
 		
 	}
-	private class addProductListener implements ActionListener{
+	private class QuitButtonListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
+			System.exit(1);
 		}
 		
 	}
-	
-	
 	
 }
+	
+	
+	
+	
+	
+
